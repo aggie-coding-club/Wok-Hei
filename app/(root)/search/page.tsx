7410ca2b76c3ca9f1c26007a6fe1b0fc3c1079ae
kaitonishikawa <kai.nishikawa808@gmail.com>
2024-10-18 00:18:17 -0500

    make header for search

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -27,11 +27,11 @@ const Search = () => {
     }
   };
   return (
-    <div>
-      search
-      <div>
-        <input value={prompt} type="text" onChange={(e) => setPrompt(e.target.value)} placeholder="Enter a prompt lol.." />
-        <div onClick={handleGenerate}>click!</div>
+    <div className='search-main h-screen'>
+      <div className='w-full bg-slate-100 p-8 flex justify-end align-middle gap-2.5'>
+        <div className='mr-auto text-3xl font-semibold'>Search</div>
+        <img onClick={handleGenerate} src="https://www.svgrepo.com/show/522266/search.svg" className='h-6 self-center cursor-pointer transition-transform transform hover:scale-110'/>
+        <input value={prompt} type="text" className="border rounded-full p-1 pl-3" onChange={(e) => setPrompt(e.target.value)} placeholder="Enter a prompt lol.."/>
       </div>
       
       <div>{ans}</div>
