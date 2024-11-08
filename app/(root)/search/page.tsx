23a89d17510f214b8adcc2a70295824d9c0eaaae
kaitonishikawa <kai.nishikawa808@gmail.com>
2024-11-07 23:42:14 -0600

    average color of deez nuts

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -9,7 +9,6 @@ const Search = () => {
   const [isOpen, setIsOpen] = useState(false)
   const [currentRecipe, setCurrentRecipe] = useState<[string[], string[], string]>([[''], [''], '']);
 
-  const openModal = () => setIsOpen(true)
   const closeModal = () => setIsOpen(false)
 
   const handleGenerate = async () => {
@@ -38,8 +37,16 @@ const Search = () => {
     new foodItemClass(
       'Brownies', 
       'https://kitchen335co.com/wp-content/uploads/2023/03/fudgy-brownies-blog-1-of-2.jpg',
-      'bg-amber-900',
-      'hover:bg-amber-900',
+      290,
+      ['beens', 'greens', 'potatoes', 'tomatoes', 'chicken', 'turkey', 'lamb', 'ham'],
+      ['take the dog behind the house', 'put lead in the dog', 'take intestines out', 'skin Skippy', 'preheat oven to 350 degrees farenheit'],
+      10,
+      20,
+      0,
+    ),
+    new foodItemClass(
+      'Brownies', 
+      'https://kitchen335co.com/wp-content/uploads/2023/03/fudgy-brownies-blog-1-of-2.jpg',
       290,
       ['flour', 'eggs', 'choco', 'chip', 'dog'],
       ['take the dog behind the house', 'put lead in the dog', 'take intestines out', 'skin Skippy', 'preheat oven to 350 degrees farenheit'],
@@ -50,8 +57,6 @@ const Search = () => {
     new foodItemClass(
       'Brownies', 
       'https://kitchen335co.com/wp-content/uploads/2023/03/fudgy-brownies-blog-1-of-2.jpg',
-      'bg-amber-900',
-      'hover:bg-amber-900',
       290,
       ['flour', 'eggs', 'choco', 'chip', 'dog'],
       ['take the dog behind the house', 'put lead in the dog', 'take intestines out', 'skin Skippy', 'preheat oven to 350 degrees farenheit'],
@@ -62,8 +67,6 @@ const Search = () => {
     new foodItemClass(
       'Brownies', 
       'https://kitchen335co.com/wp-content/uploads/2023/03/fudgy-brownies-blog-1-of-2.jpg',
-      'bg-amber-900',
-      'hover:bg-amber-900',
       290,
       ['flour', 'eggs', 'choco', 'chip', 'dog'],
       ['take the dog behind the house', 'put lead in the dog', 'take intestines out', 'skin Skippy', 'preheat oven to 350 degrees farenheit'],
@@ -72,22 +75,8 @@ const Search = () => {
       0,
     ),
     new foodItemClass(
-      'Brownies', 
-      'https://kitchen335co.com/wp-content/uploads/2023/03/fudgy-brownies-blog-1-of-2.jpg',
-      'bg-amber-900',
-      'hover:bg-amber-900',
-      290,
-      ['flour', 'eggs', 'choco', 'chip', 'dog'],
-      ['take the dog behind the house', 'put lead in the dog', 'take intestines out', 'skin Skippy', 'preheat oven to 350 degrees farenheit'],
-      10,
-      20,
-      0,
-    ),
-    new foodItemClass(
-      'Brownies', 
-      'https://kitchen335co.com/wp-content/uploads/2023/03/fudgy-brownies-blog-1-of-2.jpg',
-      'bg-amber-900',
-      'hover:bg-amber-900',
+      'Papaya', 
+      'https://www.stroke.org/-/media/Images/News/2023/October-2023/1013EIOLIPapaya_SC.jpg?sc_lang=en',
       290,
       ['flour', 'eggs', 'choco', 'chip', 'dog'],
       ['take the dog behind the house', 'put lead in the dog', 'take intestines out', 'skin Skippy', 'preheat oven to 350 degrees farenheit'],
@@ -98,28 +87,34 @@ const Search = () => {
     new foodItemClass(
       'Pumpkins', 
       'https://www.washingtonpost.com/resizer/R6HrS-hkVTLgVqrZdnP5354l1XE=/arc-anglerfish-washpost-prod-washpost/public/VAAKMD4TGKOT5HF4I74MI5LXMI.jpg',
-      'bg-amber-700' ,
-      'hover:bg-amber-700',
       290,
-      ['flour', 'eggs', 'nutmeg', 'cinnamon', 'dog'],
+      ['flour', 'eggs', 'nutmeg', 'cinnamon', 'dog', 'sugar', 'baking soda'],
+      ['take the dog behind the house', 'put lead in the dog', 'take intestines out', 'skin Skippy', 'preheat oven to 350 degrees farenheit'],
+      10,
+      20,
+      0,
+    ),
+    new foodItemClass(
+      'Brownies', 
+      'https://kitchen335co.com/wp-content/uploads/2023/03/fudgy-brownies-blog-1-of-2.jpg',
+      290,
+      ['flour', 'eggs', 'choco', 'chip', 'dog'],
+      ['take the dog behind the house', 'put lead in the dog', 'take intestines out', 'skin Skippy', 'preheat oven to 350 degrees farenheit'],
+      10,
+      20,
+      0,
+    ),
+    new foodItemClass(
+      'Pumpkins', 
+      'https://plus.unsplash.com/premium_photo-1727266867745-0927e5ebd6fe?q=80&w=2832&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      290,
+      ['flour', 'eggs', 'nutmeg', 'cinnamon', 'dog', 'sugar', 'baking soda'],
       ['take the dog behind the house', 'put lead in the dog', 'take intestines out', 'skin Skippy', 'preheat oven to 350 degrees farenheit'],
       10,
       20,
       0,
     ),
   ]
-  // foodItemsList.push(new foodItemClass(
-  //   'Brownies', 
-  //   'https://kitchen335co.com/wp-content/uploads/2023/03/fudgy-brownies-blog-1-of-2.jpg',
-  //   'bg-amber-900',
-  //   'hover:bg-amber-900',
-  //   290,
-  //   ['flour', 'eggs', 'choco', 'chip', 'dog'],
-  //   ['take the dog behind the house', 'shoot the dog', 'gut the dog', 'skin the dog', 'preheat oven to 350 degrees farenheit'],
-  //   10,
-  //   20,
-  //   0,
-  // ))
 
   return (
     <>
@@ -130,85 +125,11 @@ const Search = () => {
         <input value={prompt} type="text" className="border rounded-full p-1 pl-3" onChange={(e) => setPrompt(e.target.value)} placeholder="Enter a prompt lol.."/>
       </div>      
       <div>{ans}</div>
-      <div className='mt-5 flex flex-wrap gap-5 self-center'>
-        {/* <Brownies 
-          name='Brownies' 
-          link='https://kitchen335co.com/wp-content/uploads/2023/03/fudgy-brownies-blog-1-of-2.jpg'
-          color='bg-amber-900' 
-          hover='hover:bg-amber-900'
-          cal={290}
-          ingredient={['flour', 'eggs', 'choco', 'chip', 'dog']}
-          protein={10}
-          carb={20}
-          fat={0}
-          expanded={false}
-        ></Brownies>
-        <Brownies 
-          name='Brownies' 
-          link='https://kitchen335co.com/wp-content/uploads/2023/03/fudgy-brownies-blog-1-of-2.jpg'
-          color='bg-amber-900' 
-          hover='hover:bg-amber-900'
-          cal={290}
-          ingredient={['flour', 'eggs', 'choco', 'chip', 'dog']}
-          protein={10}
-          carb={20}
-          fat={0}
-          expanded={false}
-        ></Brownies>
-        <Brownies 
-          name='Brownies' 
-          link='https://kitchen335co.com/wp-content/uploads/2023/03/fudgy-brownies-blog-1-of-2.jpg'
-          color='bg-amber-900' 
-          hover='hover:bg-amber-900'
-          cal={290}
-          ingredient={['flour', 'eggs', 'choco', 'chip', 'dog']}
-          protein={10}
-          carb={20}
-          fat={0}
-          expanded={false}
-        ></Brownies>
-        <Brownies 
-          name='Brownies' 
-          link='https://kitchen335co.com/wp-content/uploads/2023/03/fudgy-brownies-blog-1-of-2.jpg'
-          color='bg-amber-900' 
-          hover='hover:bg-amber-900'
-          cal={290}
-          ingredient={['flour', 'eggs', 'choco', 'chip', 'dog']}
-          protein={10}
-          carb={20}
-          fat={0}
-          expanded={false}
-        ></Brownies>
-        <Brownies 
-          name='Brownies' 
-          link='https://kitchen335co.com/wp-content/uploads/2023/03/fudgy-brownies-blog-1-of-2.jpg'
-          color='bg-amber-900' 
-          hover='hover:bg-amber-900'
-          cal={290}
-          ingredient={['flour', 'eggs', 'choco', 'chip', 'dog']}
-          protein={10}
-          carb={20}
-          fat={0}
-          expanded={false}
-        ></Brownies>
-        <Brownies 
-          name='Brownies' 
-          link='https://www.washingtonpost.com/resizer/R6HrS-hkVTLgVqrZdnP5354l1XE=/arc-anglerfish-washpost-prod-washpost/public/VAAKMD4TGKOT5HF4I74MI5LXMI.jpg'
-          color='bg-amber-700' 
-          hover='hover:bg-amber-700'
-          cal={290}
-          ingredient={['flour', 'eggs', 'choco', 'chip', 'dog']}
-          protein={10}
-          carb={20}
-          fat={0}
-          expanded={false}
-        ></Brownies> */}
+      <div className='mt-4 grid lg:grid-cols-3 2xl:grid-cols-5 xl:grid-cols-4 grid-cols-2 gap-4 place-items-center'>
         {foodItemsList.map((item) => (
           <Brownies
             name = {item.name}
             link = {item.link}
-            color = {item.color}
-            hover = {item.hover}
             cal = {item.cal}
             ingredient = {item.ingredients}
             instructions = {item.instructions}
